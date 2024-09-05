c7f2706d593dd3474e86ce48573122a7db718716
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2024-09-05 21:05:31 +0300

    second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,13 @@
 // DOM Elements
-const formElement = document.querySelector(".popup");
-const nameInput = document.getElementById("name");
-const jobInput = document.getElementById("job");
-const newName = document.querySelector(".profile__name");
-const newJob = document.querySelector(".profile__job");
 const cardsContainer = document.querySelector(".cards");
-const inputUrl = document.getElementById("pic-link");
-const inputName = document.getElementById("place-name");
-const photoText = document.querySelector(".popup__photo-text");
-const photo = document.querySelector("#photo");
-const popupProfile = document.querySelector(".popup_type_profile");
-const popupAdd = document.querySelector(".popup_type_card");
-const popupPhoto = document.querySelector(".popup_type_photo");
+const cardTemplate = document.getElementById("card-template").content;
 
 // Initial setup
 renderInitialCards();
 
 // Render initial cards
 function renderInitialCards() {
-  initialCards.forEach((item) => renderCard(createCard(item)));
+  initialCards.forEach((item) => renderCard(createCard(item, deleteCard)));
 }
 
 // Prepend a card to the container
@@ -26,9 +15,13 @@ function renderCard(cardElement) {
   cardsContainer.prepend(cardElement);
 }
 
+// Global delete card handler
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // Create a card with the necessary listeners
-function createCard(item) {
-  const cardTemplate = document.getElementById("card-template").content;
+function createCard(item, deleteHandler) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector("#delete-btn");
   const cardPhoto = cardElement.querySelector(".card__image");
@@ -38,7 +31,8 @@ function createCard(item) {
   cardPhoto.alt = item.name;
   cardPhoto.src = item.link;
 
-  deleteButton.addEventListener("click", () => cardElement.remove());
+  // Attach the passed deleteHandler to the delete button
+  deleteButton.addEventListener("click", () => deleteHandler(cardElement));
 
   return cardElement;
 }
